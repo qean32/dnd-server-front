043392e2bc3c78a6c750ee779d6a206a76507739
zxccursed <asobolev925@gmail.com>
2025-09-25 14:13:18 +0400

    dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style/App.css'
+import './style/app.css'
+import './style/import.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <>zxc</>
+    <h1 className="text-xl font-bold overline">
+      Hello world!
+    </h1>
   </React.StrictMode>,
 )
